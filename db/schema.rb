2a1995b538e0ba86171a750fb605f25f677f19f6
John Paolo <paolo716@gmail.com>
2019-10-21 11:28:01 +0800

    Add exercise model and _form partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_045347) do
+ActiveRecord::Schema.define(version: 2019_10_21_025042) do
 
   create_table "exercises", force: :cascade do |t|
     t.integer "duration_in_min"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_10_19_045347) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
