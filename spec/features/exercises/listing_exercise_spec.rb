cdffe43b4beae87ab0a532bd0785fa5f85e46549
John Paolo <paolo716@gmail.com>
2019-10-22 14:32:16 +0800

    Display user's workout on index page

---
+++ spec/features/exercises/listing_exercise_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.feature "Listing Exercises" do
+    before do
+        @john = User.create!(email: "jack@example.com", password: "password")
+        login_as(@john)
+
+        @e1 = @john.exercises.create(duration_in_min: 20, 
+                                        workout: "My body buildig activity", 
+                                        workout_date: Date.today) 
+
+        @e2 = @john.exercises.create(duration_in_min: 55, 
+                                        workout: "Weight lifting", 
+                                        workout_date: 2.days.ago)    
+    end
+
+    scenario "shows user's workout for last 7 days" do
+        visit "/"
+
+        click_on 'My Lounge'
+
+        expect(page).to have_content(@e1.duration_in_min)  
+        expect(page).to have_content(@e1.workout)  
+        expect(page).to have_content(@e1.workout_date)  
+
+        expect(page).to have_content(@e2.duration_in_min)  
+        expect(page).to have_content(@e2.workout)  
+        expect(page).to have_content(@e2.workout_date)  
+
+        # expect(page).not_to have_content(@e3.duration_in_min)  
+        # expect(page).not_to have_content(@e3.workout)  
+        # expect(page).not_to have_content(@e3.workout_date)  
+
+    end
+end
